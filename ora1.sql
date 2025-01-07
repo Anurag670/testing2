fd9347d92958b2c9a046066e6b0989b03c68d5fc
Anurag670 <Anurag670>
2025-01-07 18:00:35 +0530

    again

--- ora1.sql
+++ ora1.sql
@@ -2,17 +2,19 @@
 -- ER/Studio Data Architect SQL Code Generation
 -- Project :      Model41.DM1
 --
--- Date Created : Tuesday, January 07, 2025 17:55:15
+-- Date Created : Tuesday, January 07, 2025 18:00:35
 -- Target DBMS : Oracle 23ai
 --
 
+DROP TABLE QADec23.Entity1 CASCADE CONSTRAINTS
+;
 -- 
 -- TABLE: QADec23.Entity1 
 --
 
 CREATE TABLE QADec23.Entity1(
     a           CHAR(10),
-    A_varchr    VARCHAR2(10),
+    A_varchr    VARCHAR2(10 CHAR),
     b           CHAR(10),
     c           CHAR(10)
 )
