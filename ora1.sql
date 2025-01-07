e2046d1cffc95bac1d85a51242fb28c8f44cfa66
Anurag670 <Anurag670>
2025-01-07 18:03:23 +0530

    first

---
+++ ora1.sql
@@ -0,0 +1,22 @@
+--
+-- ER/Studio Data Architect SQL Code Generation
+-- Project :      Model41.DM1
+--
+-- Date Created : Tuesday, January 07, 2025 18:03:23
+-- Target DBMS : Oracle 23ai
+--
+
+-- 
+-- TABLE: QADec23.Entity1 
+--
+
+CREATE TABLE QADec23.Entity1(
+    a           CHAR(10),
+    A_varchr    VARCHAR2(10 CHAR),
+    b           CHAR(10),
+    c           CHAR(10)
+)
+;
+
+
+
