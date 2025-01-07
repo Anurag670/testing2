303134d3e1444b37a313159689ee85e1daabc3c4
Anurag670 <Anurag670>
2025-01-07 17:55:15 +0530

    commit

---
+++ ora1.sql
@@ -0,0 +1,22 @@
+--
+-- ER/Studio Data Architect SQL Code Generation
+-- Project :      Model41.DM1
+--
+-- Date Created : Tuesday, January 07, 2025 17:55:15
+-- Target DBMS : Oracle 23ai
+--
+
+-- 
+-- TABLE: QADec23.Entity1 
+--
+
+CREATE TABLE QADec23.Entity1(
+    a           CHAR(10),
+    A_varchr    VARCHAR2(10),
+    b           CHAR(10),
+    c           CHAR(10)
+)
+;
+
+
+
