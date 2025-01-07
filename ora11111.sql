eb48db6e1f796097efcc8db27e3177376c0044de
Anurag670 <Anurag670>
2025-01-07 18:10:07 +0530

    one more

---
+++ ora11111.sql
@@ -0,0 +1,21 @@
+--
+-- ER/Studio Data Architect SQL Code Generation
+-- Project :      check_constraint_bug.DM1
+--
+-- Date Created : Tuesday, January 07, 2025 18:10:07
+-- Target DBMS : Oracle 23ai
+--
+
+-- 
+-- TABLE: TEST_TABLE 
+--
+
+CREATE TABLE TEST_TABLE(
+    TEST_PK     VARCHAR2(12)    NOT NULL,
+    TEST_COL    VARCHAR2(12)    NOT NULL,
+    PRIMARY KEY (TEST_PK)
+)
+;
+
+
+
