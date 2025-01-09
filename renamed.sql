6bfa6c657eadfd1ee2c63afed6d02f1d1dc4eafc
Anurag670 <Anurag670>
2025-01-09 13:17:48 +0530

    change

---
+++ renamed.sql
@@ -0,0 +1,23 @@
+--
+-- ER/Studio Data Architect SQL Code Generation
+-- Project :      Model41.DM1
+--
+-- Date Created : Thursday, January 09, 2025 13:17:48
+-- Target DBMS : Oracle 23ai
+--
+
+-- 
+-- TABLE: QADec23.Entity1 
+--
+
+CREATE TABLE QADec23.Entity1(
+    a           CHAR(10),
+    A_varchr    VARCHAR2(10),
+    b           CHAR(10),
+    c           CHAR(10)
+)
+;
+
+
+
+
