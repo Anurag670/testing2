1bfb84616050d395a46fee90d8ba5f3889afc126
Anurag670 <Anurag670>
2025-01-14 17:37:55 +0530

    test1

---
+++ gbq1.sql
@@ -0,0 +1,24 @@
+/*
+ * ER/Studio Data Architect SQL Code Generation
+ * Project :      DATA MODEL
+ *
+ * Date Created : Tuesday, January 14, 2025 17:37:55
+ * Target DBMS : Google BigQuery
+ */
+
+/* 
+ * TABLE: anurag_gbq.new1 
+ */
+
+CREATE TABLE anurag_gbq.new1(
+    a    STRING(10)    NOT NULL,
+    b    STRING(10),
+    c    STRING(10),
+    d    STRING(10),
+    PRIMARY KEY (a) NOT ENFORCED
+)
+OPTIONS( 
+    friendly_name = "Entity1"
+)
+;
+
